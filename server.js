c81fe788e792ddd4b345b8de6b6402f129236421
Sharmila Jesupaul <tosharmila@gmail.com>
2016-04-16 15:11:39 -0700

    pings server every 30 seconds

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require('./src/config/mongoose');
 
 import express from 'express';
 import routes from './src/controllers/index';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import http from 'http';
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,6 +17,35 @@ app.use(bodyParser.urlencoded({
 app.set('port', (process.env.PORT || 5000));
 
 routes(app);
+
+// handle any route to ping the server
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+const host = (process.env.NODE_ENV === 'production' ? 'drdick-api.herokuapp.com' : 'localhost')
+const port = (process.env.NODE_ENV === 'production' ? null : app.get('port'));
+
+// ping server every 30 seconds
+setInterval(() => {
+  const start = new Date().getTime();
+  console.log('Pinging server...');
+  http.get({
+    host: host,
+    path: '/ping',
+    port: port
+  }, (res) => {
+    var body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      console.log(body);
+      let responseTime = new Date().getTime() - start;
+      console.log('Server response in', responseTime, 'milliseconds');
+    });
+  })
+}, 30000);
 
 app.listen(app.get('port'), () => {
   console.log('Listening on port', app.get('port'));
